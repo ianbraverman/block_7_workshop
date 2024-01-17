48f4e79519e04af1de7403b6da736452349b5e63
Ian <ianbraverman@hotmail.com>
2024-01-17 15:05:18 -0500

    created starting string variable, and logged it. then created each numbers equation.

---
+++ index.js
@@ -0,0 +1,3 @@
+const opening_sentence =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log(opening_sentence);
