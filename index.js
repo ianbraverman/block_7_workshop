5f0f750c47890dbbb8288179344c151c45631e46
Ian <ianbraverman@hotmail.com>
2024-01-17 15:08:24 -0500

    created alert with all 3 numbers in an alert

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const opening_sentence =
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 console.log(opening_sentence);
+const number_1 = 5 * 3 - 5;
+const number_2 = 100 / 2 - 20 + 10;
+const number_3 = 10 * (6 + 4) - 61;
+alert(number_1 + " - " + number_2 + " - " + number_3);
