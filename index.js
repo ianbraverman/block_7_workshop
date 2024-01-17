84637ee21ea190e86b0a89d0ae0496dc8c06008b
Ian <ianbraverman@hotmail.com>
2024-01-17 15:11:10 -0500

    added comments in the code to explain steps

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+/*
+first create opening sentence that gives you string saying you will recieve this code.
+*/
 const opening_sentence =
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 console.log(opening_sentence);
+/*
+after logging that, create the 3 variables that are combinations of equations that create the numbers 10 - 40 - 39
+*/
 const number_1 = 5 * 3 - 5;
 const number_2 = 100 / 2 - 20 + 10;
 const number_3 = 10 * (6 + 4) - 61;
+/*
+then create an alert on the screen that says the 3 variables in sequence with dashes in between the numbers
+*/
 alert(number_1 + " - " + number_2 + " - " + number_3);
